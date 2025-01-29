170a1c97db221885d67f7c2c2d3587d45ddff30d
AbdulHadi03 <Rayan.Hadi003@gmail.com>
2025-01-29 09:42:02 -0600

    updating anchor tag of  contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,11 +6,11 @@ function Contact() {
         <div className="contact-container">
             <h1>Contact</h1>
             <p>LinkedIn: <a href="https://www.linkedin.com/in/mohammed--abdulhadi/">My LinkedIn Profile</a></p>
-            <p>GitHub: <a href="https://github.com/AbdulHadi03" target="_blank">My GitHub Profile</a></p>
+            <p>GitHub: <a href="https://github.com/AbdulHadi03" target="_blank" rel="noopener noreferrer">My GitHub Profile</a></p>
             <p>Email: moha208@uic.edu</p>
             <p>Phone: +12245987230</p>
         </div>
     );
-};
+}
 
 export default Contact;
